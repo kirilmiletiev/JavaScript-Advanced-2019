1447d8fa8279b43d75d08d7afbad9248a715a5a0
KirilMiletiev <kirilmiletiev@gmail.com>
2019-02-28 23:11:24 +0200

    lab

--- Lab jQuery/02. Search-In-List/search.js
+++ Lab jQuery/02. Search-In-List/search.js
@@ -1,3 +1,15 @@
 function search() {
-   // TODO:
+
+   let list = $('li').toArray();
+   let searchText = document.getElementById('searchText');
+   let counter = 0;
+
+   for (let li of list) {
+      if (li.textContent.includes(searchText.value)) {
+         li.style = "font-weight: bold;";
+        counter++;
+      }
+   }
+    document.getElementById('result').textContent = counter.toString();
+
 }
